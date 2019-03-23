808a10d316eddf3bb8732cabcca27ace06a3112e
Matt Whitehead <matthew.s.whitehead-1@ou.edu>
2019-03-23 15:31:23 -0500

    completed GamePieceAppearance class

--- lab8/src/GamePieceAppearance.java
+++ lab8/src/GamePieceAppearance.java
@@ -2,4 +2,12 @@
 
 public class GamePieceAppearance {
 
+	private Color color;
+	private Shape shape;
+	
+	GamePieceAppearance(Color color, Shape shape){
+		this.color = color;
+		this.shape = shape;
+	}
+	
 }
