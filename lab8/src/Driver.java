980fbd24ebb1e0c6f807f25d465678a7a9e0965f
Matt <matthew.s.whitehead-1@ou.edu>
2019-03-24 15:06:50 -0500

    successful test in driver class

--- lab8/src/Driver.java
+++ lab8/src/Driver.java
@@ -4,6 +4,14 @@ public class Driver {
 
 	public static void main(String[] args) {
 		
+		BoardGame game1 = new BoardGame();
+		
+		game1.addPlayer("Matt", GamePiece.BLUE_BOOT, Location.BALLROOM);
+		game1.addPlayer("Ryan", GamePiece.BLUE_RACER, Location.DINING_ROOM);
+		game1.movePlayer("Matt", Location.LIBRARY);
+		
+		System.out.println(game1.getPlayerWithGamePiece(GamePiece.BLUE_BOOT));
+		System.out.println(game1.getGamePiecesAtLocation(Location.DINING_ROOM));
 		
 		
 	}
