acbdaa28cabad6ee4aa922a428fabe3217698695
Matt <matthew.s.whitehead-1@ou.edu>
2019-03-24 16:08:27 -0500

    More changes

--- lab8/src/BoardGameTest.java
+++ lab8/src/BoardGameTest.java
@@ -33,4 +33,78 @@ public class BoardGameTest {
 		game1.movePlayer("Matt", Location.BILLIARD_ROOM);
 		Assert.assertEquals("Wrong location", Location.BILLIARD_ROOM, game1.playerLocation.get("Matt"));
 	}
+<<<<<<< HEAD
+=======
+	
+	//return to figure out why this wont pass
+	@Test
+	public void moveTwoPlayersTest() {
+		String[] players = {"Matt", "Ryan"};
+        Location[] locations = {Location.DINING_ROOM, Location.STUDY};        
+        BoardGame game1 = new BoardGame();
+        
+        game1.addPlayer("Matt", GamePiece.BLUE_BOOT, Location.BALLROOM);
+        game1.addPlayer("Ryan", GamePiece.BLUE_RACER, Location.BILLIARD_ROOM);
+        game1.addPlayer("Katie", GamePiece.RED_RACER, Location.BILLIARD_ROOM);
+
+        String[] players2 = {"Katie", "Matt"};
+        //game1.movePlayer("Matt", Location.BILLIARD_ROOM);
+
+        Assert.assertEquals(new String[] {"Matt", "Ryan"}, game1.moveTwoPlayers(players, locations));
+        Assert.assertEquals(new String[] {"Katie", "Matt"}, game1.moveTwoPlayers(players2, locations));
+	}
+	
+	@Test
+	public void getPlayerLocationTest() {
+		BoardGame game1 = new BoardGame();
+		
+		game1.addPlayer("Matt", GamePiece.BLUE_BOOT, Location.BALLROOM);
+		game1.movePlayer("Matt", Location.BILLIARD_ROOM);
+		Assert.assertEquals(game1.playerLocation.get("Matt"), game1.getPlayerLocation("Matt"));
+		
+	}
+	
+	@Test
+	public void getPlayersAtLocationTest() {
+		String[] players = {"Matt", "Ryan"};
+		Location[] locations = {Location.BALLROOM, Location.BALLROOM};
+		BoardGame game1 = new BoardGame();
+		
+		game1.addPlayer("Matt", GamePiece.BLUE_BOOT, Location.BALLROOM);
+		game1.addPlayer("Ryan", GamePiece.BLUE_RACER, Location.BILLIARD_ROOM);
+		game1.addPlayer("Katie", GamePiece.GREEN_BOOT, Location.CONSERVATORY);
+		
+		game1.moveTwoPlayers(players, locations);
+		Assert.assertEquals("Wrong Players", "Matt", game1.getPlayersAtLocation(Location.BALLROOM).get(0));
+		Assert.assertEquals("Wrong Players", "Ryan", game1.getPlayersAtLocation(Location.BALLROOM).get(1));
+
+	}
+	
+	@Test
+	public void getGamePiecesAtLocationTest() {
+		String[] players = {"Matt", "Ryan"};
+        Location[] locations = {Location.BALLROOM, Location.BALLROOM};
+        BoardGame game1 = new BoardGame();
+        
+        game1.addPlayer("Matt", GamePiece.BLUE_BOOT, Location.BALLROOM);
+        game1.addPlayer("Ryan", GamePiece.BLUE_RACER, Location.BILLIARD_ROOM);
+        game1.addPlayer("Katie", GamePiece.MAGENTA_RACER, Location.CONSERVATORY);
+        
+        game1.moveTwoPlayers(players, locations);
+        Assert.assertEquals("Wrong pieces", GamePiece.BLUE_BOOT, game1.getGamePiecesAtLocation(Location.BALLROOM).get(0));
+        Assert.assertEquals("Wrong pieces", GamePiece.BLUE_RACER, game1.getGamePiecesAtLocation(Location.BALLROOM).get(1));
+	}
+	
+	@Test
+    public void getPlayersTest() 
+    {
+        BoardGame bg = new BoardGame();
+        bg.addPlayer("Matt", GamePiece.BLUE_BOOT, Location.BALLROOM);
+        bg.addPlayer("Ryan", GamePiece.BLUE_RACER, Location.BILLIARD_ROOM);
+        bg.addPlayer("Katie", GamePiece.MAGENTA_RACER, Location.CONSERVATORY);
+        
+        Assert.assertEquals("Wrong location", 3, bg.getPlayers().size());
+        Assert.assertEquals("Wrong location", "Matt", bg.getPlayers().stream().findFirst().get());
+    }
+>>>>>>> ebfa6c2... More changes
 }
